1bde5783f7f3d9737eec61b1681cdfc91e06d290
Xiansong Wu <wuxiansong0125@gmail.com>
2023-06-22 22:19:34 +0800

    feat(pkg): add options interface

--- pkg/app/options.go
+++ pkg/app/options.go
@@ -8,4 +8,15 @@ import cliflag "github.com/marmotedu/component-base/pkg/cli/flag"
 
 type CliOptions interface {
 	Flags() (fss cliflag.NamedFlagSets)
+	Validate() []error
+}
+
+// CompleteableOptions abstracts options which can be completed.
+type CompleteableOptions interface {
+	Complete() error
+}
+
+// PrintableOptions abstracts options which can be printed.
+type PrintableOptions interface {
+	String() string
 }
