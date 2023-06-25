608c87ed8caca44d5ea6fa7e5a170ce9cf0c79a1
Xiansong Wu <wuxiansong0125@gmail.com>
2023-06-25 20:52:39 +0800

    feat(apiserver): add store

---
+++ internal/apiserver/store/mysql/mysql.go
@@ -0,0 +1,81 @@
+package mysql
+
+import (
+	"fmt"
+	"github.com/marmotedu/errors"
+	"github.com/ngsin/iam-learning/internal/apiserver/store"
+	genericoptions "github.com/ngsin/iam-learning/internal/pkg/options"
+	"github.com/ngsin/iam-learning/pkg/db"
+	"gorm.io/gorm"
+	"sync"
+)
+
+type datastore struct {
+	db *gorm.DB
+}
+
+func (ds *datastore) Users() store.UserStore {
+	return newUsers(ds)
+}
+
+func (ds *datastore) Secrets() store.SecretStore {
+	return newSecrets(ds)
+}
+
+func (ds *datastore) Policies() store.PolicyStore {
+	return newPolicies(ds)
+}
+
+func (ds *datastore) PolicyAudits() store.PolicyAuditStore {
+	return newPolicyAudits(ds)
+}
+
+func (ds *datastore) Close() error {
+	db, err := ds.db.DB()
+	if err != nil {
+		return errors.Wrap(err, "get gorm db instance failed")
+	}
+
+	return db.Close()
+}
+
+var (
+	mysqlFactory store.Factory
+	once         sync.Once
+)
+
+// GetMySQLFactoryOr create mysql factory with the given config.
+func GetMySQLFactoryOr(opts *genericoptions.MySQLOptions) (store.Factory, error) {
+	if opts == nil && mysqlFactory == nil {
+		return nil, fmt.Errorf("failed to get mysql store fatory")
+	}
+
+	var err error
+	var dbIns *gorm.DB
+	once.Do(func() {
+		options := &db.Options{
+			Host:                  opts.Host,
+			Username:              opts.Username,
+			Password:              opts.Password,
+			Database:              opts.Database,
+			MaxIdleConnections:    opts.MaxIdleConnections,
+			MaxOpenConnections:    opts.MaxOpenConnections,
+			MaxConnectionLifeTime: opts.MaxConnectionLifeTime,
+			LogLevel:              opts.LogLevel,
+			//Logger:                logger.New(opts.LogLevel),
+		}
+		dbIns, err = db.New(options)
+
+		// uncomment the following line if you need auto migration the given models
+		// not suggested in production environment.
+		// migrateDatabase(dbIns)
+
+		mysqlFactory = &datastore{dbIns}
+	})
+
+	if mysqlFactory == nil || err != nil {
+		return nil, fmt.Errorf("failed to get mysql store fatory, mysqlFactory: %+v, error: %w", mysqlFactory, err)
+	}
+
+	return mysqlFactory, nil
+}
---
+++ internal/apiserver/store/mysql/policy_audit.go
@@ -0,0 +1,24 @@
+package mysql
+
+import (
+	"context"
+	"gorm.io/gorm"
+	"time"
+)
+
+type policyAudit struct {
+	db *gorm.DB
+}
+
+func newPolicyAudits(ds *datastore) *policyAudit {
+	return &policyAudit{ds.db}
+}
+
+// ClearOutdated clear data older than a given days.
+func (p *policyAudit) ClearOutdated(ctx context.Context, maxReserveDays int) (int64, error) {
+	date := time.Now().AddDate(0, 0, -maxReserveDays).Format("2006-01-02 15:04:05")
+
+	d := p.db.Exec("delete from policy_audit where deletedAt < ?", date)
+
+	return d.RowsAffected, d.Error
+}
